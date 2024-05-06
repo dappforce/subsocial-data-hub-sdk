e292ee1715b21c9e35c13a3e18a8cc9af5f7f9c6
mckrava <mckrava2@gmail.com>
2024-05-06 12:07:43 -0600

    feat: added LinkedIdentityExternalProviderDetails type

--- src/types/call/identity.ts
+++ src/types/call/identity.ts
@@ -5,24 +5,25 @@ export interface SynthCreateLinkedIdentityCallParsedArgs {
   provider: IdentityProvider;
 }
 
+export interface LinkedIdentityExternalProviderDetails {
+  id: string;
+  provider: IdentityProvider;
+  farcasterCustodyAddress?: string;
+  farcasterVerifiedEthAddresses?: string[];
+  farcasterVerifiedSolAddresses?: string[];
+  farcasterSignerUuid?: string;
+  evmProofMsg?: string;
+  evmProofMsgSig?: string;
+}
+
 export interface SynthInitLinkedIdentityCallParsedArgs {
   session: string;
   id?: string;
-  externalProvider?: {
-    id: string;
-    provider: IdentityProvider;
-  };
+  externalProvider?: LinkedIdentityExternalProviderDetails;
 }
 
 export interface SynthAddLinkedIdentityExternalProviderCallParsedArgs {
-  externalProvider: {
-    id: string;
-    provider: IdentityProvider;
-    farcasterCustodyAddress?: string;
-    farcasterVerifiedEthAddresses?: string[];
-    farcasterVerifiedSolAddresses?: string[];
-    farcasterSignerUuid?: string;
-  };
+  externalProvider: LinkedIdentityExternalProviderDetails;
 }
 
 export interface SynthDeleteLinkedIdentityCallParsedArgs {
