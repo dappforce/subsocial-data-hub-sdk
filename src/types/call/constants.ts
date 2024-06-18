bdb2ac3c67d20ea43b4a98643cb5eb2a33cd0687
mckrava <mckrava2@gmail.com>
2024-06-18 13:17:16 -0400

    feat: added types for synth_gamification_update_task_status

--- src/types/call/constants.ts
+++ src/types/call/constants.ts
@@ -95,6 +95,8 @@ export const socialCallName = {
     'synth_gamification_add_tapping_activity_states',
   synth_gamification_claim_entrance_daily_reward:
     'synth_gamification_claim_entrance_daily_reward',
+  synth_gamification_update_task_status:
+    'synth_gamification_update_task_status',
 
   synth_social_profile_add_referrer_id: 'synth_social_profile_add_referrer_id',
   synth_add_post_view: 'synth_add_post_view',
--- src/types/call/gamification.ts
+++ src/types/call/gamification.ts
@@ -1,3 +1,8 @@
+export enum GamificationTaskName {
+  JOIN_TELEGRAM_GROUP = 'JOIN_TELEGRAM_GROUP',
+  JOIN_TELEGRAM_CHANNEL = 'JOIN_TELEGRAM_CHANNEL'
+}
+
 export interface GamificationTapEnergyState {
   value: number;
   timestamp: string;
@@ -10,4 +15,9 @@ export interface GamificationTapsState {
 export interface SynthGamificationAddTappingActivityStatesCallParsedArgs {
   tapsState?: GamificationTapsState;
   energyState?: GamificationTapEnergyState;
+}
+
+export interface SynthGamificationUpdateTaskStatusCallParsedArgs {
+  taskName: GamificationTaskName;
+  externalProviderId: string;
 }
